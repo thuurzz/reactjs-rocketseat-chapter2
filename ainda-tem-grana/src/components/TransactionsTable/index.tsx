668b4928edfbc821c3fcf7163f8515648f4f458d
Arthur Vinicius <thuur.vss@gmail.com>
2022-07-22 21:02:14 -0300

    wip: configuração do cliente axios

--- ainda-tem-grana/src/components/TransactionsTable/index.tsx
+++ ainda-tem-grana/src/components/TransactionsTable/index.tsx
@@ -1,11 +1,10 @@
 import { useEffect } from "react";
+import { api } from "../../services/api";
 import { Container } from "./styles";
 
 export function TransactionTable() {
   useEffect(() => {
-    fetch("http://localhost:3000/api/transactions")
-      .then((res) => res.json())
-      .then((data) => console.log(data));
+    api.get("transactions").then((response) => console.log(response.data));
   }, []);
 
   return (
