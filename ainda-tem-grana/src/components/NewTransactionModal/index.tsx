7213fa81993986f1de0b8bc389cfb37ed85266a8
Arthur Vinicius <thuur.vss@gmail.com>
2022-07-23 20:31:09 -0300

    wip: estrutura do formulário

--- ainda-tem-grana/src/components/NewTransactionModal/index.tsx
+++ ainda-tem-grana/src/components/NewTransactionModal/index.tsx
@@ -1,4 +1,5 @@
 import Modal from "react-modal";
+import { Container } from "./styles";
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -10,8 +11,20 @@ export function NewTransactionModal({
   onClose,
 }: NewTransactionModalProps) {
   return (
-    <Modal isOpen={isOpen} onRequestClose={onClose}>
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={onClose}
+      overlayClassName={"react-modal-overlay"}
+      className={"react-modal-content"}
+    >
       <h2>Cadastrar Transação</h2>
+
+      <Container>
+        <input type="text" placeholder="Título" />
+        <input type="number" placeholder="Valor" />
+        <input type="text" placeholder="Categoria" />
+        <button type="submit">Cadastrar</button>
+      </Container>
     </Modal>
   );
 }
--- ainda-tem-grana/src/styles/global.ts
+++ ainda-tem-grana/src/styles/global.ts
@@ -1,4 +1,3 @@
-// import { createGlobalStyle } from "styled-components";
 import * as styled from "styled-components";
 
 export const GlobalStyle = styled.createGlobalStyle`
@@ -27,12 +26,21 @@ export const GlobalStyle = styled.createGlobalStyle`
     -webkit-font-smoothing: antialiased;
   }
 
-  body, input, textarea, button {
-    font-family: 'Poppins', sans-serif;
+  body,
+  input,
+  textarea,
+  button {
+    font-family: "Poppins", sans-serif;
     font-weight: 400;
   }
 
-  h1, h2, h3, h4, h5, h6, strong {
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6,
+  strong {
     font-weight: 600;
   }
 
@@ -52,5 +60,25 @@ export const GlobalStyle = styled.createGlobalStyle`
   [disabled] {
     opacity: 0.6;
     cursor: not-allowed;
+  }
+
+  .react-modal-overlay {
+    background-color: rgba(0, 0, 0, 0.5);
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    left: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .react-modal-content {
+    width: 100%;
+    max-width: 576px;
+    background: var(--background);
+    padding: 3rem;
+    position: relative;
+    border-radius: 0.25rem;
   }
 `;
